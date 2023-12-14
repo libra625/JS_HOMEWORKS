3bb57fad5b20b394528dd635437f309dbfd22245
libra625 <18.12.17.moyo@gmail.com>
2023-12-15 00:49:03 +0200

    done home task 1

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const userName = prompt("Enter your name, please");
+
+/*
+didn't work how I wanted it
+const userName = prompt("Enter your name, please", "John");
+*/
+
+alert("Hello, " + userName + "! How are you?");
