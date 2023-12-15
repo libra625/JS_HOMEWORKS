81912eb3f06b8a2e14fe1f908f6164c96d0b9b6b
libra625 <18.12.17.moyo@gmail.com>
2023-12-15 15:45:22 +0200

    done home task 2

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// task1
+let name = "Jane";
+let admin;
+
+name = "John";
+
+admin = name;
+
+alert("Admin is " + admin);
+
+// task2
+let Earth;
+let visitorName;
+//еhe expressions above can be const
+
+// task3
+let tenInNumericAndString = "10" + 10;
+//concatenation
+//еhe expressions above can be const
+
+console.log("Тип змінної:", typeof tenInNumericAndString);
+console.log("Значення змінної:", tenInNumericAndString);
