070dc48849e939c20559ccb4247cb926860ce39b
libra625 <18.12.17.moyo@gmail.com>
2023-12-17 21:01:58 +0200

    changed let to const

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,16 @@
 // let number10 = prompt("Enter first number ");
 // let number20 = prompt("Enter second number ");
 
-let number1 = parseFloat(prompt("Enter first number "));
-let number2 = parseFloat(prompt("Enter second number "));
+const number1 = parseFloat(prompt("Enter first number "));
+const number2 = parseFloat(prompt("Enter second number "));
 
 // console.log("You entered " + number10 + " and " + number20);
 console.log("You entered " + number1 + " and " + number2);
 
-let resultOfSum = number1 + number2;
-let resultOfSubtract = number1 - number2;
-let resultOfMultiply = number1 * number2;
-let resultOfDivide = number1 / number2;
+const resultOfSum = number1 + number2;
+const resultOfSubtract = number1 - number2;
+const resultOfMultiply = number1 * number2;
+const resultOfDivide = number1 / number2;
 
 console.log(number1 + " + " + number2 + " = " + resultOfSum);
 console.log(number1 + " - " + number2 + " = " + resultOfSubtract);
