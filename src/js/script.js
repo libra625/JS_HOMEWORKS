262e1fc456e2c3ad9b36d8a54db5c0c25c427a8e
libra625 <18.12.17.moyo@hmail.com>
2024-01-01 14:54:14 +0200

    updated js files

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const firstNumber = parseFloat(prompt("Enter first number", "3"));
+const secondNumber = parseFloat(prompt("Enter second number", "5"));
+
+alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber} \n
+       ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber} \n
+       ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber} \n
+       ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber} \n
+       `);
