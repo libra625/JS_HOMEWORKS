c8e49353926f0e11a839a7c7115a6ad70ac930ce
libra625 <18.12.17.moyo@hmail.com>
2024-01-01 15:19:35 +0200

    updated js files

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const number1 = parseFloat(prompt("Enter 1st number", "2"));
+const number2 = parseFloat(prompt("Enter 1st number", "4"));
+const number3 = parseFloat(prompt("Enter 1st number", "5"));
+
+alert(`Average of ${number1}, ${number2} and ${number3} = ${(number1 + number2 + number3)/3}`);
