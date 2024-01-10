b0e06bbd3faa53662dc7da70c58cd47f9af41159
libra625 <18.12.17.moyo@gmail.com>
2024-01-10 03:45:14 +0200

    updated js file(script.js) and REAME.md

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const array = [1, 2, 3, 4, 5];
+
+//task1
+let sumOfArray = 0;
+
+for (let i = 0; i < array.length; i++) {
+    sumOfArray += array[i];
+}
+console.log(`Sum of the array = ${sumOfArray}`);
+
+//task2
+let sumOfArraySquareElements = 0;
+
+for (let i = 0; i < array.length; i++) {
+    sumOfArraySquareElements += array[i] ** 2;
+}
+console.log(`Sum of the array = ${sumOfArraySquareElements}`);
