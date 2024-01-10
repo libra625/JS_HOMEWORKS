147c105600df204e6cf4b4427134fb78f73d6523
libra625 <18.12.17.moyo@gmail.com>
2024-01-10 20:06:48 +0200

    updated js file(script.js) fixed 1 for primary number

--- src/js/script.js
+++ src/js/script.js
@@ -6,17 +6,17 @@ while (isNaN(userNumber) || userNumber === 0){
     userNumber = +prompt("Please Enter your number to check for lowest divisor , before you entered something strange", "8");
 }
 
-if (userNumber >= 1){
-    if (userNumber === 1) {
-        console.log("Your number ${userNumber} is primary ");
-    } else {
+if(userNumber === 1) {
+    console.log("number is 1")
+}
+
+if (userNumber > 1){
         for (let i = 2; i <= userNumber; i++ ) {
             if (userNumber % i === 0) {
                 console.log(`Divisor for ${userNumber} was found it is ${i}`);
                 break;
             }
         }
-    }
 } else {
     console.log("Ba_NaN");
 }
