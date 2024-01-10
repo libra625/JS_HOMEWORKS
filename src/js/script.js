6c4a6b60b8a631a692b3efb68a1e68903d923eca
libra625 <18.12.17.moyo@gmail.com>
2024-01-10 04:27:08 +0200

    updated js file(script.js) and REAME.md

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let userNumber = +prompt("Enter your number to check for lowest divisor ", "8");
+
+while (isNaN(userNumber) || userNumber === 0){
+    userNumber = +prompt("Please Enter your number to check for lowest divisor , before you entered something strange", "8");
+}
+
+if (userNumber >= 1){
+    for (let i = 2; i <= userNumber; i++ ) {
+        if (i === userNumber || userNumber === 1){
+            console.log("Your number ${userNumber} is primary");
+            break
+        }
+
+        if (userNumber % i === 0) {
+            console.log(`Divisor for ${userNumber} was found it is ${i}`);
+            break;
+        }
+    }
+
+} else console.log("Ba_NaN");
