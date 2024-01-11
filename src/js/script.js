ed7764c53d3deebf3c8665537cea24e828eacb65
libra625 <18.12.17.moyo@gmail.com>
2024-01-12 00:22:35 +0200

    updated js file(script.js)

--- src/js/script.js
+++ src/js/script.js
@@ -32,34 +32,48 @@ for (let i = 1; i ** 2 <= userNumber; i++) {
 //task4
 console.log("4th TASK");
 
-let primaryMark = false;
 userNumber = +prompt("Enter a number for task 4", "9");
 
 while (isNaN(userNumber) || userNumber === 0) {
     userNumber = +prompt("You entered something strange! Please enter a number for task 4 correctly", "9");
 }
 
-for (let i = 2; i <= userNumber; i++) {
-    if (userNumber <= 1) {
-        break;
-    }
+if (userNumber <= 1) {
+    console.log(`Your number (${userNumber}) less or equals 1`);
+} else {
+    for (let i = 2; i <= userNumber; i++) {
+        if (userNumber % i ===0 && userNumber === i) {
+            console.log(`${userNumber} is a primary number`);
+            break;
+        }
 
-    if (userNumber % i ===0 && userNumber === i) {
-        console.log(`${userNumber} is a primary number`);
-        break;
-    }
-
-    if (userNumber % i === 0) {
-        console.log(`${userNumber} is not a primary number! It was divided by ${i}`);
-        break;
+        if (userNumber % i === 0) {
+            console.log(`${userNumber} is not a primary number! It was divided by ${i}`);
+            break;
+        }
     }
 }
 
+//task5
+console.log("5th TASK");
 
+userNumber = +prompt("Enter a number for task 5", "9");
 
+while (isNaN(userNumber) || userNumber === 0) {
+    userNumber = +prompt("You entered something strange! Please enter a number for task 5 correctly", "9");
+}
 
+for (let i = 0; i <= userNumber; i++) {
+    if (3 ** i > userNumber) {
+        console.log("Your number can't be 3 in some degree")
+        break;
+    }
 
-
+    if (3 ** i === userNumber) {
+        console.log(`Your number can be 3 in some degree. And this degree is ${i}`)
+        break;
+    }
+}
 
 
 
