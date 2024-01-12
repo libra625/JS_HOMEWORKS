4ce947bf5fde3879bbe2c5c1e6830c7778b74396
libra625 <18.12.17.moyo@gmail.com>
2024-01-12 05:04:17 +0200

    updated js file(script.js) and README.md

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function numberToDegree(number, degree = 1) {
+
+    if (isNaN(number) || isNaN(degree))
+        return "error incorrect data";
+
+    const temporaryNumber = BigInt(+number);
+    const temporaryDegree = BigInt(+degree);
+
+    return BigInt(temporaryNumber ** temporaryDegree);
+}
+
+const number = +prompt("Enter number which need to be risen to a certain power", "2");
+const degree = +prompt("Enter a degree for your number", "8");
+
+alert(`Result : ${numberToDegree(number, degree)}`);
