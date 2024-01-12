54043c482953d2cb4ceb3b1784b96c1f62197a7d
libra625 <18.12.17.moyo@gmail.com>
2024-01-12 15:18:33 +0200

    updated js file(script.js) and README.md

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+function padString(string= "", lengthRequired, characterToChange = "", positionRight = true) {
+        if (string.length === 0) {
+            return "error no string";
+        }
+
+        if (isNaN(lengthRequired) || lengthRequired === null || typeof lengthRequired !== "number") {
+            return "required length is strange";
+        }
+
+
+
+        if (typeof positionRight !== "boolean") {
+            return "error not a boolean (position) of addition";
+        }
+
+        let temporaryString = string;
+
+        if (temporaryString.length >= lengthRequired) {
+            return temporaryString.slice(0, lengthRequired);
+        } else if (characterToChange.length !== 1) {
+            return "error no character for empty space";
+        }
+        
+        if (positionRight) {
+            while ( temporaryString.length < lengthRequired) {
+                temporaryString += characterToChange;
+            }
+            
+            return temporaryString;
+        } else {
+            let amountOfDifference = lengthRequired - temporaryString.length;
+            let partOfTemporaryString = "";
+            
+            while (amountOfDifference) {
+                partOfTemporaryString += characterToChange;
+                amountOfDifference--;
+            }
+            
+            return partOfTemporaryString + temporaryString;
+        }
+}
+
+console.log(padString('привіт', 8, '') );
+console.log(padString('привіт', 6, '*', false));
+console.log(padString('привіт', 2));
