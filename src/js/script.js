055c7c9293c2d30a95a5568fb0de8bc75e9bc927
libra625 <18.12.17.moyo@gmail.com>
2024-01-12 22:35:10 +0200

    updated js file(script.js) and README.md

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function deleteNegativeFromArray(array) {
+    const positiveArray = []
+
+    if (!array.length) {
+        return "Array is empty";
+    }
+    
+    for (let i = 0; i <= array.length; i++) {
+        if (array[i] > 0) {
+            positiveArray.push(array[i]);
+        }
+    }
+    
+    return !positiveArray.length ? null : positiveArray;
+}
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+console.log(deleteNegativeFromArray(arr));
