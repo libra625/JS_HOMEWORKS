c5f520eb32c72480486c1c8f1764e90e5b112718
libra625 <18.12.17.moyo@gmail.com>
2024-01-13 20:45:30 +0200

    updated js file(script.js) made it a function

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const averagePotatoWeight = 75;
-const potatoesRequiredFor1Liter = 4;
-const potato1KilogramCost = 13;
+function calculateCostOfRequiredPotato(litersOfSoup) {
+    const averagePotatoWeight = 75;
+    const potatoesRequiredFor1Liter = 4;
+    const potato1KilogramCost = 13;
+
+    const amountOfPotatoToCook = ((litersOfSoup * potatoesRequiredFor1Liter * averagePotatoWeight) / 1000);
+
+   return `You need ${amountOfPotatoToCook} kg to cook ${litersOfSoup} liters but you can buy just ${Math.ceil(amountOfPotatoToCook)} and it will cost you ${Math.ceil(amountOfPotatoToCook) * potato1KilogramCost} UAH`
+}
 
 let litersOfSoup = +prompt("How many liters of soup do you want to cook?", "48");
 
@@ -10,8 +16,6 @@ while (isNaN(litersOfSoup) || litersOfSoup === 0){
     litersOfSoup = +prompt("Please Enter How many liters of soup do you want to cook? ,It must be a number", "48");
 }
 
-const amountOfPotatoToCook = ((litersOfSoup * potatoesRequiredFor1Liter * averagePotatoWeight) / 1000);
+console.log(calculateCostOfRequiredPotato(litersOfSoup));
 
-console.log(`You need ${amountOfPotatoToCook} kg to cook ${litersOfSoup} liters but you can buy just ${Math.ceil(amountOfPotatoToCook)} and it will cost you ${Math.ceil(amountOfPotatoToCook) * potato1KilogramCost} UAH`);
-
-alert(`You need ${amountOfPotatoToCook} kg to cook ${litersOfSoup} liters but you can buy just ${Math.ceil(amountOfPotatoToCook)} and it will cost you ${Math.ceil(amountOfPotatoToCook) * potato1KilogramCost} UAH`);
+alert(calculateCostOfRequiredPotato(litersOfSoup));
