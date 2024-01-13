8352e1ea1470d66751073cc63c8542c22e851bca
libra625 <18.12.17.moyo@gmail.com>
2024-01-13 20:50:29 +0200

    updated js file(script.js) replaced check to function

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,21 @@
 'use strict';
 
 function calculateCostOfRequiredPotato(litersOfSoup) {
+
+    if (isNaN(litersOfSoup) || litersOfSoup === 0) {
+        return "error";
+    }
+
     const averagePotatoWeight = 75;
     const potatoesRequiredFor1Liter = 4;
     const potato1KilogramCost = 13;
 
     const amountOfPotatoToCook = ((litersOfSoup * potatoesRequiredFor1Liter * averagePotatoWeight) / 1000);
 
-   return `You need ${amountOfPotatoToCook} kg to cook ${litersOfSoup} liters but you can buy just ${Math.ceil(amountOfPotatoToCook)} and it will cost you ${Math.ceil(amountOfPotatoToCook) * potato1KilogramCost} UAH`
+   return `You need ${amountOfPotatoToCook} kg to cook ${litersOfSoup} liters but you can buy just ${Math.ceil(amountOfPotatoToCook)} and it will cost you ${Math.ceil(amountOfPotatoToCook) * potato1KilogramCost} UAH`;
 }
 
 let litersOfSoup = +prompt("How many liters of soup do you want to cook?", "48");
-
-while (isNaN(litersOfSoup) || litersOfSoup === 0){
-    litersOfSoup = +prompt("Please Enter How many liters of soup do you want to cook? ,It must be a number", "48");
-}
 
 console.log(calculateCostOfRequiredPotato(litersOfSoup));
 
