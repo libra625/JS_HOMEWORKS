e23eaf5212b23ecb45d98f8a5da1eaaefa0ec4ac
libra625 <18.12.17.moyo@gmail.com>
2024-01-16 00:35:19 +0200

    updated js file(script.js) and README.md

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let array = [];
+
+const uniqueNumber = function () {
+    let randomNumber = Math.floor(Math.random()*100);
+
+    if (!array.includes(randomNumber)) {
+        array.push(randomNumber);
+        return randomNumber;
+    } else while (array.includes(randomNumber)) {
+        randomNumber = Math.floor(Math.random()*100 + 1);
+    }
+    
+    array.push(randomNumber);
+    
+    return randomNumber;
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(uniqueNumber());
+}
