565e9ae809c0dbb1a74c9af718cc4138fc926de0
libra625 <18.12.17.moyo@hmail.com>
2024-01-22 22:02:21 +0200

    add 2nd solution

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,40 @@
 'use strict';
 
-let array = [];
+const array1 = [];
+const array2 = [];
 
 const uniqueNumber = function () {
     let randomNumber = Math.floor(Math.random()*100 + 1);
 
-    if (!array.includes(randomNumber)) {
-        array.push(randomNumber);
+    if (!array1.includes(randomNumber)) {
+        array1.push(randomNumber);
         return randomNumber;
     }
 
-    while (array.includes(randomNumber)) {
+    while (array1.includes(randomNumber)) {
         randomNumber = Math.floor(Math.random()*100 + 1);
     }
-    
-    array.push(randomNumber);
+
+    array1.push(randomNumber);
     
     return randomNumber;
 }
 
+const uniqueNumber2 = function () {
+    let randomNumber = Math.floor(Math.random()*100 + 1);
+
+    if (!array2.includes(randomNumber)) {
+        array2.push(randomNumber);
+        return randomNumber;
+    }
+
+    return uniqueNumber2();
+}
+
 for (let i = 1; i <= 100; i++) {
     console.log(uniqueNumber());
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(uniqueNumber2());
 }
