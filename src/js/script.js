01ec20d97964f26c9ee54041fc8885c8a1f5347a
libra625 <18.12.17.moyo@hmail.com>
2024-01-25 17:41:06 +0200

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const createUser = function (name, age) {
+  const user = {
+    name,
+    age,
+    greet () {
+      console.log(`Hello my name is ${user.name}`)
+    }
+  }
+
+  user.greet2 = function () {
+    console.log(`Hello my name is ${this.name}`)
+  }
+
+  return user
+}
+
+const user1 = createUser('Іван', 25)
+const user2 = createUser('Марія', 30)
+
+user1.greet()
+user1.greet2()
+
+user2.greet()
+user2.greet2()
