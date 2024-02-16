5aceb506a595d7099631460a06cf7cf93fae9af2
libra625 <18.12.17.moyo@gmail.com>
2024-02-16 02:06:50 +0200

    updated js file(script.js) and README.md

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+'use strict'
+
+Function.prototype.myBind = function (context, ...args) {
+    const fn = this
+
+    return function (...newArgs) {
+        if (context === null || context === undefined) {
+            context = globalThis
+        }
+
+        return fn.apply(context, [...args, ...newArgs])
+    }
+}
+
+const someObj = {
+    name: 'John',
+    surname: 'Doe'
+}
+
+const example = function (a, b) {
+    console.log(this, a, b)
+}
+
+const exampleWithContext = example.bind(someObj, 'string1', 'string2')
+const exampleWithContext2 = example.bind(someObj, 'string1', 'string2')
+
+exampleWithContext(10, 20)
+exampleWithContext2(10, 20)
+example(10, 20)
+
+console.log(exampleWithContext === exampleWithContext2)
