e55e44fff37fb7733a27c4d380bc9ff58875e8bd
libra625 <18.12.17.moyo@gmail.com>
2024-03-18 14:48:26 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,10 @@
             }, {})
 
             localStorage.setItem(dataKey, JSON.stringify(data))
+
+            setTimeout(() => {
+                window.location.href = 'http://localhost:63342/JS_HOMEWORKS/src/sub.html'
+            }, 3000)
         })
     }
 
